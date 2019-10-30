ed7516744eb2fec0a8eaea779f1310c3bba93005
Ravishankar Rao <rbrao@Ravishankars-MBP.local>
2019-10-30 21:59:39 +0530

    Re-arranging contents

--- spog-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:/opt/spog/node/bin
-audit_output=`/sbin/auditctl -l`
-if [ "$audit_output" = "No Rules" ] || [ "$audit_output" = "" ]; then
-	service auditd restart
-fi
