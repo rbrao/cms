a040ff9e50f99cc3cb7a71113705c89bab0ba6c0
Ravishankar Rao <rbrao@Ravishankars-MBP.local>
2019-10-31 20:26:42 +0530

    Added school folder; Minor edits

--- bin/spog-ftp.sh
+++ bin/spog-ftp.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-SRC=`ls -tr /opt/spog/sbin/*.db|tail -1`
-DBFILE=`ls -tr /opt/spog/sbin/*.db|tail -1|awk -F/ '{print \$NF}'`
-DISTRICT=`grep ^district /opt/spog/etc/config.yaml |awk -F: '{print $NF}'|sed s/^\ //g`
-SUBDIV=`grep ^subdivision /opt/spog/etc/config.yaml |awk -F: '{print $NF}'|sed s/^\ //g`
+DISTRICT=`grep -i ^district /opt/spog/etc/config.yaml |awk -F: '{print $NF}'|sed s/^\ //g`
+SUBDIV=`grep -i ^subdivision /opt/spog/etc/config.yaml |awk -F: '{print $NF}'|sed s/^\ //g`
+SCHOOL=`grep -i ^school /opt/spog/etc/config.yaml |awk -F: '{print $NF}'|sed s/^\ //g`
 
 /opt/spog/node/bin/bytenode /opt/spog/sbin/copydb.jsc
+SRC=`ls -tr /opt/spog/sbin/*.db|tail -1`
+DBFILE=`ls -tr /opt/spog/sbin/*.db|tail -1|awk -F/ '{print \$NF}'`
 
 ftp -np -v 139.59.58.35 << EOT
 bin
 user spogcms spog123
 prompt
-mkdir "$DISTRICT"/"$SUBDIV"/$DBFILE
-put $SRC "$DISTRICT"/"$SUBDIV"/$DBFILE
+mkdir "$DISTRICT"
+mkdir "$DISTRICT"/"$SUBDIV"
+mkdir "$DISTRICT"/"$SUBDIV"/"$SCHOOL"
+put $SRC "$DISTRICT"/"$SUBDIV"/"$SCHOOL"/$DBFILE
 bye
 EOT
